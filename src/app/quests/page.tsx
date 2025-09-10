97842300ee31dba4315cc4d7da4327ec0a886c54
RD500 <146438600+RD500@users.noreply.github.com>
2025-09-11 00:19:13 +0530

    Update page.tsx

--- src/app/quests/page.tsx
+++ src/app/quests/page.tsx
@@ -1,6 +1,6 @@
 
 'use client';
-
+export const dynamic = 'force-dynamic';
 import { MainLayout } from '@/components/layout/main-layout';
 import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
